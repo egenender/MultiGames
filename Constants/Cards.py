54c1bdc05895dbbbf3ec75130e34eb56689d2ee1
leviatas <31702513+leviatas@users.noreply.github.com>
2018-06-15 10:00:40 -0300

    Update Cards.py

--- Constants/Cards.py
+++ Constants/Cards.py
@@ -2,386 +2,385 @@ cartas_aventura = {
     0.5 : {
         "nombre" : "Nombre",
         "plastilla" : "1",
-        "fila" : "0" 
+        "fila" : "0",
         "columna" : "0"        
     },
     1 : {
         "nombre" : "Nombre",
         "plastilla" : "1",
-        "fila" : "0" 
+        "fila" : "0",
         "columna" : "1"      
     },
     2 : {
         "nombre" : "Nombre",
         "plastilla" : "1",
-        "fila" : "0" 
+        "fila" : "0",
         "columna" : "2"     
     },
     3 : {
         "nombre" : "Nombre",
         "plastilla" : "1",
-        "fila" : "1" 
+        "fila" : "1",
         "columna" : "0"
     },
     4 : {
         "nombre" : "Nombre",
         "plastilla" : "1",
-        "fila" : "1" 
+        "fila" : "1",
         "columna" : "1"      
     },
     5 : {
         "nombre" : "Nombre",
         "plastilla" : "1",
-        "fila" : "1" 
+        "fila" : "1",
         "columna" : "2"     
     },
     6 : {
         "nombre" : "Nombre",
         "plastilla" : "1",
-        "fila" : "2" 
+        "fila" : "2",
         "columna" : "0"
     },
     7 : {
         "nombre" : "Nombre",
         "plastilla" : "1",
-        "fila" : "2" 
+        "fila" : "2",
         "columna" : "1"
     },
     8 : {
         "nombre" : "Nombre",
         "plastilla" : "1",
-        "fila" : "2" 
+        "fila" : "2",
         "columna" : "2"
     },
     9 : {
         "nombre" : "Nombre",
         "plastilla" : "2",
-        "fila" : "0" 
+        "fila" : "0",
         "columna" : "0"
         
     },
     10" : {
         "nombre" : "Nombre",
         "plastilla" : "2",
-        "fila" : "0" 
+        "fila" : "0",
         "columna" : "1"
     },
     10.5 : {
         "nombre" : "Nombre",
         "plastilla" : "2",
-        "fila" : "0" 
+        "fila" : "0",
         "columna" : "2"
     },
     11 : {
         "nombre" : "Nombre",
         "plastilla" : "2",
-        "fila" : "1" 
+        "fila" : "1",
         "columna" : "0"
     },
     12 : {
         "nombre" : "Nombre",
         "plastilla" : "2",
-        "fila" : "1" 
+        "fila" : "1",
         "columna" : "1"
     },
     13 : {
         "nombre" : "Nombre",
         "plastilla" : "2",
-        "fila" : "1" 
+        "fila" : "1",
         "columna" : "2"
     },
     14 : {
         "nombre" : "Nombre",
         "plastilla" : "2",
-        "fila" : "2" 
+        "fila" : "2",
         "columna" : "0"
     },
     15 : {
         "nombre" : "Nombre",
         "plastilla" : "2",
-        "fila" : "2" 
-        "columna" : "1",        
+        "fila" : "2",
+        "columna" : "1"
     },
     16 : {
         "nombre" : "Nombre",
         "plastilla" : "2",
-        "fila" : "2" 
-        "columna" : "2",        
+        "fila" : "2",
+        "columna" : "2"
     },
     17 : {
         "nombre" : "Nombre",
         "plastilla" : "3",
-        "fila" : "0" 
-        "columna" : "0",
-        
+        "fila" : "0",
+        "columna" : "0"
     },
     18 : {
         "nombre" : "Nombre",
         "plastilla" : "3",
-        "fila" : "0" 
-        "columna" : "1",        
+        "fila" : "0", 
+        "columna" : "1"
     },
     19 : {
         "nombre" : "Nombre",
         "plastilla" : "3",
-        "fila" : "0" 
-        "columna" : "2",        
+        "fila" : "0",
+        "columna" : "2"
     },
     20 : {
         "nombre" : "Nombre",
         "plastilla" : "3",
-        "fila" : "1" 
-        "columna" : "0",        
+        "fila" : "1", 
+        "columna" : "0"
     },
     20.5 : {
         "nombre" : "Nombre",
         "plastilla" : "3",
-        "fila" : "1" 
-        "columna" : "1",        
+        "fila" : "1", 
+        "columna" : "1"
     },
     21 : {
         "nombre" : "Nombre",
         "plastilla" : "3",
         "fila" : "1" 
-        "columna" : "2",        
+        "columna" : "2"
     },
     22 : {
         "nombre" : "Nombre",
         "plastilla" : "3",
-        "fila" : "2" 
-        "columna" : "0",        
+        "fila" : "2",
+        "columna" : "0"
     },
     23 : {
         "nombre" : "Nombre",
         "plastilla" : "3",
-        "fila" : "2" 
-        "columna" : "1",        
+        "fila" : "2", 
+        "columna" : "1"
     },
     24 : {
         "nombre" : "Nombre",
         "plastilla" : "3",
-        "fila" : "2" 
-        "columna" : "2",        
+        "fila" : "2", 
+        "columna" : "2"
     },
     25 : {
         "nombre" : "Nombre",
         "plastilla" : "4",
-        "fila" : "0" 
-        "columna" : "0",
+        "fila" : "0",
+        "columna" : "0"
         
     },
     26 : {
         "nombre" : "Nombre",
         "plastilla" : "4",
-        "fila" : "0" 
-        "columna" : "1",        
+        "fila" : "0",
+        "columna" : "1"
     },
     27 : {
         "nombre" : "Nombre",
         "plastilla" : "4",
-        "fila" : "0" 
-        "columna" : "2",        
+        "fila" : "0",
+        "columna" : "2"
     },
     28 : {
         "nombre" : "Nombre",
         "plastilla" : "4",
-        "fila" : "1" 
-        "columna" : "0",        
+        "fila" : "1",
+        "columna" : "0"
     },
     29 : {
         "nombre" : "Nombre",
         "plastilla" : "4",
-        "fila" : "1" 
-        "columna" : "1",        
+        "fila" : "1",
+        "columna" : "1"
     },
     30 : {
         "nombre" : "Nombre",
         "plastilla" : "4",
-        "fila" : "1" 
-        "columna" : "2",        
+        "fila" : "1",
+        "columna" : "2"
     },
     31 : {
         "nombre" : "Nombre",
         "plastilla" : "4",
-        "fila" : "2" 
-        "columna" : "0",        
+        "fila" : "2",
+        "columna" : "0"
     },
     32 : {
         "nombre" : "Nombre",
         "plastilla" : "4",
-        "fila" : "2" 
-        "columna" : "1",        
+        "fila" : "2",
+        "columna" : "1"
     },
     33 : {
         "nombre" : "Nombre",
         "plastilla" : "4",
-        "fila" : "2" 
-        "columna" : "2",        
+        "fila" : "2",
+        "columna" : "2"
     },
     34 : {
         "nombre" : "Nombre",
         "plastilla" : "5",
-        "fila" : "0" 
-        "columna" : "0",
+        "fila" : "0",
+        "columna" : "0"
         
     },
     35 : {
         "nombre" : "Nombre",
         "plastilla" : "5",
-        "fila" : "0" 
-        "columna" : "1",        
+        "fila" : "0",
+        "columna" : "1"
     },
     36 : {
         "nombre" : "Nombre",
         "plastilla" : "5",
-        "fila" : "0" 
-        "columna" : "2",        
+        "fila" : "0",
+        "columna" : "2"
     },
     37 : {
         "nombre" : "Nombre",
         "plastilla" : "5",
-        "fila" : "1" 
-        "columna" : "0",        
+        "fila" : "1",
+        "columna" : "0"
     },
     38 : {
         "nombre" : "Nombre",
         "plastilla" : "5",
-        "fila" : "1" 
-        "columna" : "1",        
+        "fila" : "1",
+        "columna" : "1"
     },
     39 : {
         "nombre" : "Nombre",
         "plastilla" : "5",
-        "fila" : "1" 
-        "columna" : "2",        
+        "fila" : "1",
+        "columna" : "2"
     },
     40 : {
         "nombre" : "Nombre",
         "plastilla" : "5",
-        "fila" : "2" 
-        "columna" : "0",        
+        "fila" : "2",
+        "columna" : "0"
     },
     40.5 : {
         "nombre" : "Nombre",
         "plastilla" : "5",
-        "fila" : "2" 
-        "columna" : "1",        
+        "fila" : "2",
+        "columna" : "1"
     },
     41 : {
         "nombre" : "Nombre",
         "plastilla" : "5",
-        "fila" : "2" 
-        "columna" : "2",        
+        "fila" : "2",
+        "columna" : "2"
     },
     42 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "0" 
-        "columna" : "0",
+        "fila" : "0",
+        "columna" : "0"
         
     },
     43 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "0" 
-        "columna" : "1",        
+        "fila" : "0",
+        "columna" : "1"
     },
     44 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "0" 
-        "columna" : "2",        
+        "fila" : "0", 
+        "columna" : "2"
     },
     45 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "1" 
-        "columna" : "0",        
+        "fila" : "1", 
+        "columna" : "0"
     },
     46 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "1" 
-        "columna" : "1",        
+        "fila" : "1", 
+        "columna" : "1"
     },
     47 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "1" 
-        "columna" : "2",        
+        "fila" : "1", 
+        "columna" : "2"
     },
     48 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "2" 
-        "columna" : "0",        
+        "fila" : "2", 
+        "columna" : "0"
     },
     49 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "2" 
-        "columna" : "1",        
+        "fila" : "2", 
+        "columna" : "1"
     },
     50 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "2" 
-        "columna" : "2",        
+        "fila" : "2", 
+        "columna" : "2"
     },
     50.5 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "0" 
-        "columna" : "0",
+        "fila" : "0", 
+        "columna" : "0"
         
     },
     51 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "0" 
-        "columna" : "1",        
+        "fila" : "0", 
+        "columna" : "1"
     },
     52 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "0" 
-        "columna" : "2",        
+        "fila" : "0", 
+        "columna" : "2"   
     },
     53 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "1" 
-        "columna" : "0",        
+        "fila" : "1", 
+        "columna" : "0"    
     },
     54 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "1" 
-        "columna" : "1",        
+        "fila" : "1", 
+        "columna" : "1"     
     },
     55 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "1" 
-        "columna" : "2",        
+        "fila" : "1", 
+        "columna" : "2"    
     },
     56 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "2" 
-        "columna" : "0",        
+        "fila" : "2", 
+        "columna" : "0"     
     },
     99.5 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "2" 
-        "columna" : "1",        
+        "fila" : "2", 
+        "columna" : "1"     
     },
     0 : {
         "nombre" : "Nombre",
         "plastilla" : "6",
-        "fila" : "2" 
-        "columna" : "2",        
+        "fila" : "2", 
+        "columna" : "2"      
     }, 
 }
 
