818819bfcfc54eb9c396368b2c6cef2dcce8fb0f
leviatas <31702513+leviatas@users.noreply.github.com>
2018-10-31 14:36:36 -0300

    Update DBCreate.sql

--- DBCreate.sql
+++ DBCreate.sql
@@ -7,8 +7,16 @@
     name text NOT NULL
 );
 */
-CREATE TABLE IF NOT EXISTS games_xapi_bot (
+/*CREATE TABLE IF NOT EXISTS games_xapi_bot (
     id bigint PRIMARY KEY,
     groupName TEXT NOT NULL,
     data text NOT NULL
+);*/
+DROP TABLE IF EXISTS games_xapi_bot;
+
+CREATE TABLE IF NOT EXISTS games (
+    id bigint PRIMARY KEY,
+    groupName TEXT NOT NULL,
+    tipojuego TEXT NOT NULL,
+    data TEXT NOT NULL
 );
