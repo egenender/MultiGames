39ec0736d0a09dc0ceea69b7b0edf3bccb96bd23
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-02 11:14:07 -0300

    Update Player.py

--- Boardgamebox/Player.py
+++ Boardgamebox/Player.py
@@ -15,3 +15,19 @@ class Player(object):
         self.vida_explorador_campero = 3
         self.vida_explorador_brujula = 3
         self.vida_explorador_hoja = 3
+    
+    def print_stats(self):
+        board = "--- Stats Actuales ---\n"
+        board = "--- Comida %s ---\n" % self.food
+        board = "--- Balas %s ---\n" % self.bullets
+        board = "--- vida_explorador_campero %s ---\n" % self.vida_explorador_campero
+        board = "--- vida_explorador_brujula %s ---\n" % self.vida_explorador_brujula
+        board = "--- vida_explorador_hoja %s ---\n" % self.vida_explorador_hoja
+        
+        '''for uid in player_list:
+            board += "%s tiene " % (player_list[uid].name)
+            for i in range(player_list[uid].tokens_posesion):
+                board += "\U0001F47F"            
+            board += "\n"  
+        '''
+        return board
