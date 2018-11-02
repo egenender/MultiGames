d1b9a85010cbbf66a9a61ac7a8e8601ad15e3bfb
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-02 11:17:47 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -889,7 +889,7 @@ def main():
         dp.add_handler(CommandHandler("rules", Commands.command_rules))
         dp.add_handler(CommandHandler("ping", Commands.command_ping))
         dp.add_handler(CommandHandler("symbols", Commands.command_symbols))
-        dp.add_handler(CommandHandler("stats", Commands.command_stats))
+        
         #dp.add_handler(CommandHandler("newgame", Commands.command_newgame))
         dp.add_handler(CommandHandler("startgame", Commands.command_startgame))
         dp.add_handler(CommandHandler("cancelgame", Commands.command_cancelgame))
@@ -919,8 +919,7 @@ def main():
         dp.add_handler(CommandHandler("losebullet", Commands.command_losebullet))
         dp.add_handler(CommandHandler("gainbullet", Commands.command_gainbullet))
         dp.add_handler(CommandHandler("losefood", Commands.command_losefood))
-        dp.add_handler(CommandHandler("gainfood", Commands.command_gainfood))
-        
+        dp.add_handler(CommandHandler("gainfood", Commands.command_gainfood))        
         dp.add_handler(CommandHandler("stats", Commands.command_showstats))
         
         #Testing commands
