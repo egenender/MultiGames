321a55d1e44e10c6370e219bbd2df9b789020688
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-07 12:47:44 -0300

    Update Config.py

--- Constants/Config.py
+++ Constants/Config.py
@@ -1,3 +1,4 @@
 TOKEN = "501280363:AAGr5YOn5-HBAKOKJoSSUys6NOfUC-A9PwM"
-ADMIN = [387393551, 271584582] #your telegram ID
+ADMIN = [387393551, 271584582, 6473134, 13517295] #your telegram ID
+#Leviatas Bernabe Gonza Eze
 STATS = "../stats.json"
