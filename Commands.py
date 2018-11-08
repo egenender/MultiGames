797f3361b1b00674e5a2bf08bfb7023363760e32
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-08 18:35:55 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -117,7 +117,7 @@ def execute_command(bot, update):
 	comando = regex.group(3)
 	
 	
-	bot.send_message(cid, update.message)
+	bot.send_message(cid, update.callback_query)
 	# Directamente lo ejecuto ya que tengo el argumento.
 	getattr(sys.modules[__name__], comando)(bot, update, opcion)
 	
