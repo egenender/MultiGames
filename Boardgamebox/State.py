b03731b0f5652722cabfff03f42a9f2d01c08ff2
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-09 08:23:04 -0300

    Update State.py

--- Boardgamebox/State.py
+++ Boardgamebox/State.py
@@ -1,6 +1,17 @@
 class State(object):
     """Storage object for game state"""
     def __init__(self):
+        self.acciones_carta_actual = None
+        # Marca el indice de la accion que actuialmente se esta ejecutando.
+        # Este numero aumenta cuando la opcion actual termino todos sus comandos
+        self.index_accion_actual = 0
+        # Opcion en caso de que hubiese mas de 1, que se esta ejecutando.
+        # Este numero no aumenta, se elije.
+        self.index_opcion_actual = 0
+        # Este numero va en aumento hasta que no hay mas comandos que realizar.
+        self.index_comando_actual = 0
+        
+        # Borrar luego al hacer limpieza del bot
         self.liberal_track = 0
         self.fascist_track = 0
         self.failed_votes = 0
