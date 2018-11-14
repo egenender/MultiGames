9627892e634f75db2634c52159759e4369f5ed58
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-14 10:59:17 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -833,8 +833,9 @@ def command_swap_exploration(bot, update, args):
 			bot.send_message(cid, "Se tienen que ingresar 2 argumentos")
 			return			
 		if args[0] == "Sí" or args[0] == "No":
-			bot.send_message(cid, "Por favor haga el swap Manual y luego haga /continue")
-			return "Esperar"
+			if args[0] == "Sí":
+				bot.send_message(cid, "Por favor haga el swap Manual y luego haga /continue")
+				return "Esperar"
 		else:
 			a, b =  int(args[0])-1, int(args[1])-1		
 			game.board.cartasExplorationActual[b], game.board.cartasExplorationActual[a] = game.board.cartasExplorationActual[a], game.board.cartasExplorationActual[b]		
