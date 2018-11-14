112e7c46bf7f4cea3e7af78717ae89c8eebf65ce
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-14 09:53:06 -0300

    Update Cards.py

--- Constants/Cards.py
+++ Constants/Cards.py
@@ -66,7 +66,7 @@ comandos = {
         "tipo" : "indicaciones",
         "comando" : "command_lose_compass",
         "indicacion" : "Elija que quiere hacer",
-        "indicacion_argumentos" : ["Explorador Campero", "Explorador Brujula", "Explorador Hoja", "Usar carta skill"]
+        "indicacion_argumentos" : ["Explorador Campero 2", "Explorador Brujula 1", "Explorador Hoja 2", "Usar carta skill"]
     },
     "lose_bullet" : {
         "tipo" : "automatico", # Caso especial que no se puede elegir si no se tiene
