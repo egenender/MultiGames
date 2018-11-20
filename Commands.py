437207b0f3560a3b447958b66f90961ae4b68e6a
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-20 13:51:36 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -592,7 +592,7 @@ def command_showhand(bot, update, args):
 			return
 		player = game.playerlist[uid]
 		#cid = '-1001206290323'
-		if not player.hand):
+		if not player.hand:
 			bot.send_message(cid, "El jugador no tiene cartas")
 		elsE:
 			bot.send_message(cid, "Mano jugador")
