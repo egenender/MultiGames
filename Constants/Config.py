c6c8f8b16bc2aca7da4d8cd429879fc79469b8e2
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-22 11:05:17 -0300

    Update Config.py

--- Constants/Config.py
+++ Constants/Config.py
@@ -2,3 +2,16 @@ TOKEN = "501280363:AAGr5YOn5-HBAKOKJoSSUys6NOfUC-A9PwM"
 ADMIN = [387393551, 441820689, 445782140] #your telegram ID
 #Leviatas Ale Cadogan
 STATS = "../stats.json"
+
+JUEGOS_DISPONIBLES = {        
+        "LostExpedition" : {
+                "comandos" : {
+                    "LostExpedition" : "Lost Expedition"
+                }
+        },
+        "JustOne" : {
+                "comandos" : {
+                    "JustOne" : "Just One"
+                }
+        }
+}
