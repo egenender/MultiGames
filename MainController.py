d5c4484e6974b7d3b38741262e1fc27ef9530c21
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-22 11:33:22 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -1038,9 +1038,8 @@ def main():
         # Pruebas SH
         dp.add_handler(CommandHandler("role", Commands.command_choose_posible_role))
         dp.add_handler(CallbackQueryHandler(pattern="(-[0-9]*)\*chooserole\*(.*)\*([0-9]*)", callback=Commands.callback_choose_posible_role))
-	
 	dp.add_handler(CommandHandler("role", Commands.command_configurar_partida))
-        dp.add_handler(CallbackQueryHandler(pattern="(-[0-9]*)\*choosegame\*(.*)\*([0-9]*)", callback=Commands.callback_choose_posible_role))
+	dp.add_handler(CallbackQueryHandler(pattern="(-[0-9]*)\*choosegame\*(.*)\*([0-9]*)", callback=Commands.callback_choose_posible_role))
 	
         # log all errors
         dp.add_error_handler(error)
