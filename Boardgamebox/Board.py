df66ea65f2785d2e5f52072801ec4310ad305448
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-22 16:35:28 -0300

    Update Board.py

--- Boardgamebox/Board.py
+++ Boardgamebox/Board.py
@@ -17,8 +17,8 @@ class Board(object):
         self.progreso = 1
         self.objetivoprogreso = 9
         
-        self.exploradores_team1 = []
-        self.exploradores_team2 = []
+        self.exploradores_team1 = {}
+        self.exploradores_team2 = {}
         
         self.discards = []
         self.previous = []        
