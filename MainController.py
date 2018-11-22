fd864b6066ecaf3f55d38cd39f376e51e54e79d7
leviatas <31702513+leviatas@users.noreply.github.com>
2018-11-22 11:37:13 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -935,6 +935,8 @@ def main():
         # Get the dispatcher to register handlers
         dp = updater.dispatcher
 
+	algo = ""
+	
         # on different commands - answer in Telegram
         dp.add_handler(CommandHandler("start", Commands.command_start))
         dp.add_handler(CommandHandler("help", Commands.command_help))
@@ -1038,7 +1040,7 @@ def main():
         dp.add_handler(CommandHandler("role", Commands.command_choose_posible_role))
         dp.add_handler(CallbackQueryHandler(pattern="(-[0-9]*)\*chooserole\*(.*)\*([0-9]*)", callback=Commands.callback_choose_posible_role))
 	
-	algo = ""
+	
         # log all errors
         dp.add_error_handler(error)
 
