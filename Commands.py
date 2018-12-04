6b93e5504e2813bb5b4aa0c5a39abac0595849d8
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-04 09:39:31 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -2136,18 +2136,18 @@ def command_roll(bot, update, args):
 	if args:
 		text_tirada = 'Tu tirada de ' + ' '.join(args)
 	else:
-		text_tirada = 'Tu tirada ha sido'
+		text_tirada = 'Tu tirada '
 	
 	cid = update.message.chat_id
 	tirada = random.randint(1,101)	
 	if tirada > 97:
 		tirada2 = random.randint(1,101)
-		bot.send_message(cid, "¡%s *%s!* " % (text_tirada, str(tirada+tirada2)), ParseMode.MARKDOWN)
+		bot.send_message(cid, "¡%s es *%s!* " % (text_tirada, str(tirada+tirada2)), ParseMode.MARKDOWN)
 	elif tirada < 4:
 		tirada2 = random.randint(1,101)
-		bot.send_message(cid, "¡%s *%s*!" % (text_tirada, str(tirada-tirada2)), ParseMode.MARKDOWN)
+		bot.send_message(cid, "¡%s es *%s*!" % (text_tirada, str(tirada-tirada2)), ParseMode.MARKDOWN)
 	elif tirada == 27:
 		bot.send_message(cid, "¡%s Épico!* " % text_tirada, ParseMode.MARKDOWN)
 	else:
-		bot.send_message(cid, "¡%s *%s*!" % (text_tirada, str(tirada)), ParseMode.MARKDOWN)
+		bot.send_message(cid, "¡%s es *%s*!" % (text_tirada, str(tirada)), ParseMode.MARKDOWN)
 		
