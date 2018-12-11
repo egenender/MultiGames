3808f5a045114692153c9e87fb0a3d50b0ec360c
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-11 16:10:19 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -1935,4 +1935,41 @@ def command_roll(bot, update, args):
 		player = game.playerlist[uid]
 		texthistory = "Jugador *%s* - %s" % (player.name, text_tirada)
 		game.history.append("%s" % (texthistory))
-		
+
+def command_clue(bot, update, args):
+	try:		
+		#Send message of executing command   
+		cid = update.message.chat_id
+		uid = update.message.from_user.id
+
+		# Busco en que juegos esta el jugador y agrego el historia oculto en los que este. (Futuro se
+		for game_key, game in GamesController.games.items():
+			#Solamente si el jugador esta en el partido y 
+			if uid in game.playerlist:
+				#Check if there is a current game
+				# TODO VErificar que no mande pista el jugador activo
+				if True:
+					if len(args) > 0:
+						#Data is being claimed
+						# TODO Verificar que el usuario no mande pistas con espacios.
+						claimtext = ' '.join(args)
+						#claimtexttohistory = "El jugador %s declara: %s" % (game.playerlist[uid].name, claimtext)
+						bot.send_message(uid, "Tu pista: %s fue agregada a las pistas." % (claimtext))
+						game.board.state.last_votes[uid] = answer
+						
+						# Verifico si todos los jugadores -1 pusieron pista
+						if len(game.board.state.last_votes) == len(game.player_sequence)-1:
+							MainController.review_clues(bot, game)
+					else:					
+						bot.send_message(uid, "Debes mandar un mensaje para hacer una declaración.")
+
+				else:
+					bot.send_message(uid, "No puedes hacer claim oculto sin promulgar al menos una política.")
+			else:
+				bot.send_message(uid, "No puedes hacer claim oculto si no estas en algun partido.")				
+	except Exception as e:
+		bot.send_message(uid, str(e))
+		log.error("Unknown error: " + str(e))
+	
+	
+	
