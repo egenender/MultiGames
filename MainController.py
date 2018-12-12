c64925239395b33c803f1f89db927de27bb92e4d
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-12 16:43:53 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -249,6 +249,7 @@ def callback_reviewer_confirm(bot, update):
 			game.board.state.progreso += 1
 		else:
 			# Se elimina la proxima carta del mazo.
+			bot.send_message(game.cid, "La palabra era: {0}.\nSe ha eliminado del mazo 1 carta como penalización".format(game.board.state.acciones_carta_actual), ParseMode.MARKDOWN)			
 			game.board.cartas.pop(0)
 		Commands.save(bot, game.cid)
 		start_next_round(bot, game)
