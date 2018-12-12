a95e34268c182acd1d60d5d7cda076d572ac4f4d
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-12 10:46:56 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -1739,7 +1739,7 @@ def callback_choose_posible_role(bot, update):
 
 
 def multipurpose_choose_buttons(bot, cid, uid, chat_donde_se_pregunta, comando_callback, mensaje_pregunta, opciones_botones):
-	sleep(3)
+	#sleep(3)
 	btns = []
 	# Creo los botones para elegir al usuario
 	for opcion in opciones_botones:
