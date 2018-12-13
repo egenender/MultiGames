885808f6214f5e224009344f78cb4c74d1706b3b
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-13 10:37:23 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -214,24 +214,8 @@ def after_ruta_achicada(bot, cid, uid):
 def command_hoja_ayuda(bot, update):
 	cid = update.message.chat_id	
 	game = get_game(cid)	
-	if game.tipo == 'LostExpedition':
-		help_text = "Eventos amarillos son obligatorios\n" + \
-				"Eventos rojo son obligatorios pero tenes que elegir 1\n"  + \
-				"Eventos Azules son opcionales"
-	elif game.tipo == 'JustOne':
-		help_text = "Pistas *no* validas:\n" + \
-				"*Pista con ortografia diferente.* Ej: Kamiza* para Camisa\n" + \
-				"*Palabras escritas en otro idioma.* Ej:Black para Negro\n"  + \
-				"*Una palabra de la misma familia* Ej:Principe para Princesa\n"  + \
-				"*Una palabra inventada* Ej:Cositadulz para Paster\n"  + \
-				"*Una palabra foneticamente identica.* Ej: Tuvo para Tubo\m" + \
-				"Pistas *identicas*\n" + \
-				"*Dos palabras identicas.*" + \
-				"*Variantes de una misma familia de palabras* Ej: Princesa y Principe" + \
-				"*Las variantes de una misma palabra: los plurales, diferencias de genero" + \
-				" y faltas de ortografia no cuenta como diferencias reales* Ej: Principe y Principes" + \
-				"Panadero y Panadera, Tobogán y Tovogan son identicas.",	
-	bot.send_message(cid, help_text)
+	help_text = HOJAS_AYUDA.get(game.tipo)	
+	bot.send_message(cid, help_text, ParseMode.MARKDOWN)
 	if game.tipo == 'LostExpedition':
 		bot.send_photo(cid, photo=open('/app/img/LostExpedition/Ayuda01.jpg', 'rb'))
 		bot.send_photo(cid, photo=open('/app/img/LostExpedition/Ayuda02.jpg', 'rb'))
