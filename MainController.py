f736e154b3211dad886847fd960aeea30b2f0b03
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-14 13:27:17 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -386,11 +386,14 @@ def start_next_round(bot, game):
 	# start next round if there is no winner (or /cancel)
 	# Si hubo descartes los muestro antes de comenzar el nuevo turno
 	
+	
 	try:
-		if game.board.state.removed_votes:
+		bot.send_message(ADMIN[0], game.board.state.removed_votes)
+		if game.board.state.removed_votes:game.board.state.removed_votes
 			text_eliminadas = get_pistas_eliminadas(game)			
 			bot.send_message(game.cid, text_eliminadas, ParseMode.MARKDOWN)		
 	except Exception as e:
+		bot.send_message(ADMIN[0], game.board.state.amount_shuffled)
 		bot.send_message(ADMIN[0], 'Fallo al usar removed_votes: '+str(e))
 		if game.board.state.amount_shuffled:
 			text_eliminadas = get_pistas_eliminadas(game)			
