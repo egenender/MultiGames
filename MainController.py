0bedb7ffbc16bcba84fe684d49ffa1f207f4a4c6
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-14 15:55:15 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -335,10 +335,10 @@ def callback_reviewer_confirm(bot, update):
 			game.board.discards.append(game.board.state.acciones_carta_actual)
 		else:
 			# Se elimina la proxima carta del mazo.
-			bot.send_message(game.cid, "La palabra era: *{0}*.\nSe ha eliminado del mazo 1 carta como penalización".format(game.board.state.acciones_carta_actual), ParseMode.MARKDOWN)			
+			bot.send_message(game.cid, "La palabra era: *{0}*.".format(game.board.state.acciones_carta_actual), ParseMode.MARKDOWN)			
 			game.board.discards.append(game.board.state.acciones_carta_actual)
 			# Solo descarto si hay cartas
-			if not game.board.cartas:
+			if len(game.board.cartas) != 0:
 				bot.send_message(game.cid, "Se ha *eliminado del mazo 1 carta* como penalización", ParseMode.MARKDOWN)			
 				game.board.discards.append(game.board.cartas.pop(0))
 			else:
