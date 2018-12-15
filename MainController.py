7d64a412e7f3450de2e2bbc8c9d4716fb0dd8151
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-15 00:02:50 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -192,7 +192,7 @@ def start_round_just_one(bot, game):
 	for uid in game.playerlist:
 		if uid != game.board.state.active_player.uid:
 			#bot.send_message(cid, "Enviando mensaje a: %s" % game.playerlist[uid].name)
-			mensaje = "Nueva palabra en el grupo *{1}*.\nAdivina {2}\nLa palabra es: *{0}*, propone tu pista!".format(palabra_elegida, game.groupName, game.board.state.active_player.name)
+			mensaje = "Nueva palabra en el grupo *{1}*.\nAdivina el jugador: *{2}*\nLa palabra es: *{0}*, propone tu pista!".format(palabra_elegida, game.groupName, game.board.state.active_player.name)
 			bot.send_message(uid, mensaje, ParseMode.MARKDOWN)
 			if game.board.num_players != 3:
 				mensaje = "/clue Ejemplo"
