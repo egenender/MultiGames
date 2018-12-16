8c08c4c8ac57b17df9e43b39e458b5c0eac81cca
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-15 23:21:49 -0300

    Create helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,12 @@
+__author__ = "Eduardo Peluffo"
+
+# Remueve repetidos y devuelve ambas listas
+def remove_same_elements_dict(last_votes):
+	last_votes_to_lower = {key:val.lower() for key, val in last_votes.items()}	
+	repeated_keys = []
+	valores_last_votes_to_lower = list(last_votes_to_lower.values())#last_votes_to_lower.values()
+	for key, value in last_votes_to_lower.items():
+		if valores_last_votes_to_lower.count(value) > 1:
+			repeated_keys.append(key)	
+	return {key:val for key, val in last_votes.items() if key not in repeated_keys}, {key:val for key, val in last_votes.items() if key in repeated_keys}
+
