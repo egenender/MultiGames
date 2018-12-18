4cdfa603a607743ae1008bc1fe1f6e78a8350e22
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-18 14:05:17 -0300

    Update HarryPotterCommands.py

--- GameCommands/HarryPotterCommands.py
+++ GameCommands/HarryPotterCommands.py
@@ -11,11 +11,21 @@ from time import sleep
 from Utils.helpers import helper
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, ForceReply
 
+import MainController
+import GamesController
+
+from Boardgamebox.Board import Board
+from Boardgamebox.Game import Game
+from Boardgamebox.Player import Player
+from Boardgamebox.State import State
+
+
+
 import random
 import re
+# Objetos que uso de prueba estaran en el state
 
 # Enable logging
-
 log.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=log.INFO)
@@ -34,6 +44,8 @@ conn = psycopg2.connect(
 )
 
 def command_roll(bot, update, args):	
+	import Commands
+	
 	if args:
 		text_tirada = '¡Tu tirada de ' + ' '.join(args)
 	else:
@@ -57,9 +69,9 @@ def command_roll(bot, update, args):
 	bot.send_message(cid, "%s" % (text_tirada), ParseMode.MARKDOWN)
 	
 	# Si hay un juego creado guardo en el historial
-	game = get_game(cid)
+	game = Commands.get_game(cid)
 	if game and uid in game.playerlist:
+		#bot.send_message(cid, "Grabo en base de datos", ParseMode.MARKDOWN)
 		player = game.playerlist[uid]
 		texthistory = "Jugador *%s* - %s" % (player.name, text_tirada)
 		game.history.append("%s" % (texthistory))
-
