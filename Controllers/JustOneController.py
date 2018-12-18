51986468d2751cf972f34d383352750093175a15
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-18 12:16:39 -0300

    Update JustOneController.py

--- Controllers/JustOneController.py
+++ Controllers/JustOneController.py
@@ -316,7 +316,7 @@ def get_pistas_eliminadas(game):
 
 def start_next_round(bot, game):
 	#ot.send_message(ADMIN[0], game.board.state.removed_votes)
-	log.info('start_next_round called')
+	bot.send_message(ADMIN[0], 'start_next_round called')
 	bot.send_message(ADMIN[0], game.board.state.removed_votes)
 	if game.board.state.removed_votes:
 		bot.send_message(ADMIN[0], game.board.state.removed_votes)
