fa581c04688ebf0a551aa4c8c9b2c3fd45cad3a8
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-18 14:41:50 -0300

    Update JustOneController.py

--- Controllers/JustOneController.py
+++ Controllers/JustOneController.py
@@ -123,20 +123,22 @@ def start_round_just_one(bot, game):
 	bot.send_message(game.cid, "El jugador *%s* tiene que adivinar" % active_player.name, ParseMode.MARKDOWN)
 	bot.send_message(game.cid, "El jugador *%s* revisara las pistas" % reviewer_player.name, ParseMode.MARKDOWN)
 	game.dateinitvote = datetime.datetime.now()
+	call_players_to_clue(bot, game)			
+	game.dateinitvote = datetime.datetime.now()
+	game.board.state.fase_actual = "Proponiendo Pistas"
+	Commands.save(bot, game.cid)
+
+def call_players_to_clue(bot, game):
 	for uid in game.playerlist:
 		if uid != game.board.state.active_player.uid:
 			#bot.send_message(cid, "Enviando mensaje a: %s" % game.playerlist[uid].name)
-			mensaje = "Nueva palabra en el grupo *{1}*.\nAdivina el jugador: *{2}*\nLa palabra es: *{0}*, propone tu pista!".format(palabra_elegida, game.groupName, game.board.state.active_player.name)
+			mensaje = "Nueva palabra en el grupo *{1}*.\nAdivina el jugador: *{2}*\nLa palabra es: *{0}*, propone tu pista!".format(game.board.state.acciones_carta_actual, game.groupName, game.board.state.active_player.name)
 			bot.send_message(uid, mensaje, ParseMode.MARKDOWN)
 			if game.board.num_players != 3:
 				mensaje = "/clue Ejemplo"
 			else:
 				mensaje = "/clue Ejemplo Ejemplo2"
 			bot.send_message(uid, mensaje)
-			
-	game.dateinitvote = datetime.datetime.now()
-	game.board.state.fase_actual = "Proponiendo Pistas"
-	Commands.save(bot, game.cid)
 	
 def review_clues(bot, game):
 	log.info('review_clues called')
