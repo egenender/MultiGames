09b3fbe2e1c0a4d3a3c3b974fb6a3ff9e456eb27
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-19 16:45:56 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -461,10 +461,9 @@ def main():
 	dp.add_handler(CommandHandler("tirada", Commands.command_roll, pass_args = True))
 		
 	dp.add_handler(MessageHandler(Filters.command, unknown))
-	dp.add_handler(MessageHandler(Filters.text, echo))
-	#echo_handler = MessageHandler(Filters.text, echo)
-	#dp.add_handler(echo_handler)
 	
+	#dp.add_handler(MessageHandler(Filters.text, echo))
+		
 	# log all errors
 	dp.add_error_handler(error)
 
