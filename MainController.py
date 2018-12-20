4a6ff8761adb5a0fd6021ddbc71acffafd5860f4
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-20 12:07:27 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -456,7 +456,7 @@ def main():
 	
 	# Herramientas de ADMIN
 	dp.add_handler(CommandHandler("ann", Commands.command_announce, pass_args = True))
-	dp.add_handler(CallbackQueryHandler(pattern="(-[0-9]*)\*announce\*(.*)\*([0-9]*)", callback=callback_announce))
+	dp.add_handler(CallbackQueryHandler(pattern="(-?[0-9]*)\*announce\*(.*)\*(-?[0-9]*)", callback=callback_announce))
 	
 	# Lost Expedition Commands
 	dp.add_handler(CommandHandler("newgamelostexpedition", LostExpeditionCommands.command_newgame_lost_expedition))
