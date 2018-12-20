9463237c7b14cc2cf778b3157146af4db1ec58a7
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-20 08:35:16 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -462,7 +462,7 @@ def main():
 		
 	dp.add_handler(MessageHandler(Filters.command, unknown))
 	
-	#dp.add_handler(MessageHandler(Filters.text, echo))
+	dp.add_handler(MessageHandler(Filters.text, echo))
 		
 	# log all errors
 	dp.add_error_handler(error)
