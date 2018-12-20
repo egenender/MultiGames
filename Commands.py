d3fe17d624185225e13c1b92638ff6ede5185b9c
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-20 18:02:06 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -778,7 +778,7 @@ def command_myturns(bot, update):
 	# Me improtan los juegos que; Este el jugador, hayan sido iniciados, datinivote no sea null y que cumpla reglas del tipo de juego en particular
 	all_games = {key:game for key, game in all_games_unfiltered.items() if uid in game.playerlist and game.board != None and verify_my_turn(game, uid) }
 	for game_chat_id, game in all_games.items():
-		bot.send_message(uid, "Tienes pendiente el juego en el grupo *{0}*".format(game.groupName), ParseMode.MARKDOWN)			
+		bot.send_message(uid, myturn_message( game, uid), ParseMode.MARKDOWN)			
 	if len(all_games) == 0:
 		bot.send_message(uid, "*NO* tienes partidos pendientes", ParseMode.MARKDOWN)
 	
