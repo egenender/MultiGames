93e1dce6dd73c66a7c4c911d66ea271561754a73
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-21 10:52:36 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -321,7 +321,13 @@ def unknown(bot, update):
 def add_group(bot, update):
 	groupname = update.message.chat.title
 	bot.send_message(ADMIN[0], "Entro en add new member: {}".format(groupname))
-	bot.send_message(ADMIN[0], update.message.new_chat_members)
+	#bot.send_message(ADMIN[0], update.message.new_chat_members)
+	try:
+		log.info(update.message.new_chat_members)
+	except Exception as e:
+		bot.send_message(ADMIN[0], "Error al logear chat members {0}".format(str(e)))
+		
+		
 	
 	for members in update.message.new_chat_members:
         	bot.send_message(ADMIN[0], text="{username} {id} add group".format(username=members.username, id=member.id, groupname = groupname))
