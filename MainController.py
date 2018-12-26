9789ffd038dce7c475480cf8fc6f230ecfe73ee3
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-26 10:54:41 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -482,11 +482,11 @@ def main():
 	dp.add_error_handler(error)
 
 	# Start the Bot (Usar si no es WebHook)
-	#updater.start_polling()
+	updater.start_polling()
 	
 	# Configuracion para usar Web hook
-	updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
-	updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+	#pdater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
+	#pdater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
 	
 	# Run the bot until the you presses Ctrl-C or the process receives SIGINT,
 	# SIGTERM or SIGABRT. This should be used most of the time, since
