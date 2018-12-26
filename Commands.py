fefc631a18b66ee88b7429383eefce74f35b24fd
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-26 09:41:19 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -327,7 +327,7 @@ def command_votes(bot, update):
 		bot.send_message(cid, str(e))
 
 def command_call(bot, update):
-	import GameCommands.JustoneCommands as JustoneCommands
+	import Justone.Commands as JustoneCommands
 	
 	try:
 		#Send message of executing command   
@@ -696,8 +696,8 @@ def simple_choose_buttons(bot, cid, uid, chat_donde_se_pregunta, comando_callbac
 	bot.send_message(chat_donde_se_pregunta, mensaje_pregunta, reply_markup=btnMarkup)		
 
 def command_continue(bot, update, args):
-	import GameCommands.JustoneCommands as JustoneCommands
-	import GameCommands.LostExpeditionCommands as LostExpeditionCommands
+	import Justone.Commands as JustoneCommands
+	import LostExpedition.Commands as LostExpeditionCommands
 	
 	log.info('command_continue called')
 	
