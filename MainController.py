2d98575fbab7264fc61b9ccd6ee5d1205d0453ad
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-26 10:42:12 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -364,19 +364,13 @@ def main():
 	query = "SELECT ...."
 	cur.execute(query)
 	'''
-
+	
+	NAME = "MultiGames"
+	PORT = os.environ.get('PORT')
+	
 	#PORT = int(os.environ.get('PORT', '8443'))
 	updater = Updater(TOKEN)
-
-	# DEscomentar para Webhook acordarse de cambiar el tipo de bot a web
-	'''
-	updater.start_webhook(listen="0.0.0.0",
-		      port=PORT,
-		      url_path=TOKEN,
-		      key='8ca9c17937b0699c7643b1084d97d2b40a4ceadc75f32d9914ceffcff873')
-	updater.bot.set_webhook("https://multigames.herokuapp.com/" + TOKEN)
-	'''              
-
+	
 	# Get the dispatcher to register handlers
 	dp = updater.dispatcher
 
@@ -488,8 +482,12 @@ def main():
 	dp.add_error_handler(error)
 
 	# Start the Bot (Usar si no es WebHook)
-	updater.start_polling()
-
+	#updater.start_polling()
+	
+	# Configuracion para usar Web hook
+	updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
+	updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+	
 	# Run the bot until the you presses Ctrl-C or the process receives SIGINT,
 	# SIGTERM or SIGABRT. This should be used most of the time, since
 	# start_polling() is non-blocking and will stop the bot gracefully.
