a87e87413e542a79d047c1b805b31a088a4ac708
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-28 14:14:31 -0300

    Update Game.py

--- SayAnything/Boardgamebox/Game.py
+++ SayAnything/Boardgamebox/Game.py
@@ -4,6 +4,7 @@ from random import shuffle
 
 from Boardgamebox.Game import Game as BaseGame
 from SayAnything.Boardgamebox.Player import Player
+from SayAnything.Boardgamebox.Board import Board
 #from Boardgamebox.Board import Board
 #from Boardgamebox.State import State
 
@@ -14,3 +15,6 @@ class Game(BaseGame):
 	# Creacion de player de Say Anything.
 	def add_player(self, uid, name):
 		self.playerlist[uid] = Player(uid, name)
+	def create_board(self):
+		player_number = len(game.playerlist)
+		self.board = Board(player_number, self)
