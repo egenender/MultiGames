de6e602a8a7b89f6f34bef6cb563f19c06402f04
leviatas <31702513+leviatas@users.noreply.github.com>
2019-01-04 12:27:51 -0300

    Update Cards.py

--- Arcana/Constants/Cards.py
+++ Arcana/Constants/Cards.py
@@ -1,1 +1,219 @@
-ArcanaCards = { }
+ArcanaCards = [
+  {    
+		"Título": "1-4-7",
+		"Texto": "Si uno de tus destinos es 1",
+		"Lunas": " 4 o 7",
+		"Título reverso": " y el otro no",
+		"Texto reverso": " juega el 1"
+	},
+	{
+		"Título": "Dentro de 2",
+		"Texto": "Si la diferencia entre tus destinos es 1 o 2",
+		"Lunas": " jugá uno de ellos aquí",
+		"Título reverso": "2",
+		"Texto reverso": "¿Dentro de 2?"
+	},
+	{
+		"Título": "Levantamiento",
+		"Texto": "Si uno de tus destinos es mayor que el destino más alto jugado aquí",
+		"Lunas": " jugá el otro destino aquí.",
+		"Título reverso": "4",
+		"Texto reverso": "+1"
+	},
+	{
+		"Título": "Solo",
+		"Texto": "Si ningún destino visible es exactamente 1 más o 1 menos que cualquiera de tus destinos",
+		"Lunas": " jugá uno de ellos aquí.",
+		"Título reverso": "2",
+		"Texto reverso": "+1"
+	},
+	{
+		"Título": "Igual",
+		"Texto": "Si tus dos destinos son iguales",
+		"Lunas": " jugá uno de ellos aquí.",
+		"Título reverso": "1",
+		"Texto reverso": "Repetir"
+	},
+	{
+		"Título": "Todos x4",
+		"Texto": "Si la suma de tus destinos más todos los destinos visibles es un múltiplo de 4",
+		"Lunas": " jugá uno de tus destinos aquí.",
+		"Título reverso": "3",
+		"Texto reverso": "Reubicar"
+	},
+	{
+		"Título": "Altas probabilidades",
+		"Texto": "Si tus destinos son diferentes y ambos impares",
+		"Lunas": " jugá el más alto aquí. Si solo uno es impar",
+		"Título reverso": " jugá el más bajo aquí.",
+		"Texto reverso": "3"
+	},
+	{
+		"Título": "±1",
+		"Texto": "Si uno de tus destinos es exactamente 1 más o 1 menos que el otro",
+		"Lunas": " jugá uno de ellos aquí.",
+		"Título reverso": "1",
+		"Texto reverso": "Repetir"
+	},
+	{
+		"Título": "Menor",
+		"Texto": "Si uno de tus destinos es menor que el otro",
+		"Lunas": " jugá el menor aquí",
+		"Título reverso": "3",
+		"Texto reverso": "¿Mayor que?"
+	},
+	{
+		"Título": "Libre",
+		"Texto": "Colocá uno de tus destinos aquí. No avances la condena cuando me desvanezca.",
+		"Lunas": "1",
+		"Título reverso": "1-4-7",
+		"Texto reverso": "Antes de hacer una predicción"
+	},
+	{
+		"Título": "≥11",
+		"Texto": "Si la suma de tus destinos es 11 o más",
+		"Lunas": " jugá uno de ellos aquí",
+		"Título reverso": "4",
+		"Texto reverso": "Descartar menor"
+	},
+	{
+		"Título": "Coincidencia",
+		"Texto": "Si uno de tus destinos coincide con uno visible",
+		"Lunas": " jugá tu otro destino aquí.",
+		"Título reverso": "2",
+		"Texto reverso": "Ciclar"
+	},
+	{
+		"Título": "x3",
+		"Texto": "Si la suma de tus destinos es un múltiplo de 3",
+		"Lunas": " jugá uno de ellos aquí.",
+		"Título reverso": "2",
+		"Texto reverso": "Descartar menor"
+	},
+	{
+		"Título": "5-6-7",
+		"Texto": "Si uno de tus destinos es 5",
+		"Lunas": " 6 o 7",
+		"Título reverso": " y el otro no",
+		"Texto reverso": " juega el 5"
+	},
+	{
+		"Título": "7 a 9",
+		"Texto": "Si la suma de tus destinos es 7",
+		"Lunas": " 8 o 9",
+		"Título reverso": " jugá uno de ellos aquí.",
+		"Texto reverso": "3"
+	},
+	{
+		"Título": "Adivinar",
+		"Texto": "Colocá uno de tus destinos aquí. Si lo hacés",
+		"Lunas": " tus compañeros pueden hacer una predicción extra este turno.",
+		"Título reverso": "2",
+		"Texto reverso": "¿Viejo?"
+	},
+	{
+		"Título": "Diferencia",
+		"Texto": "Si la diferencia entre tus destinos es igual a un destino visible",
+		"Lunas": " jugá el mayor aquí",
+		"Título reverso": "1",
+		"Texto reverso": "¿Igual?"
+	},
+	{
+		"Título": "Factor",
+		"Texto": "Si uno de tus destinos es el doble o el triple del otro",
+		"Lunas": " jugá uno de ellos aquí.",
+		"Título reverso": "3",
+		"Texto reverso": "5-6-7"
+	},
+	{
+		"Título": "1-2-3",
+		"Texto": "Si uno de tus destinos es 1",
+		"Lunas": " 2 o 3",
+		"Título reverso": " y el otro no",
+		"Texto reverso": " juega el 1"
+	},
+	{
+		"Título": "Sacar",
+		"Texto": "Colocá uno de tus destinos aquí",
+		"Lunas": " sacá otro destino y continuá tu turno. No podés jugar aquí nuevamente.",
+		"Título reverso": "4",
+		"Texto reverso": "Reubicar"
+	},
+	{
+		"Título": "Decir",
+		"Texto": "Colocá uno de tus destinos aquí. Si lo hacés",
+		"Lunas": " decí si tu destino restante es mayor que el que jugaste.",
+		"Título reverso": "2",
+		"Texto reverso": "1-4-7"
+	},
+	{
+		"Título": "Impar",
+		"Texto": "Si la suma de tus destinos es impar",
+		"Lunas": " jugá uno de ellos aquí.",
+		"Título reverso": "3",
+		"Texto reverso": "1-2-3"
+	},
+	{
+		"Título": "Mayor",
+		"Texto": "Si uno de tus destinos es mayor que el otro",
+		"Lunas": " jugá el mayor aquí",
+		"Título reverso": "3",
+		"Texto reverso": "¿Mayor que?"
+	},
+	{
+		"Título": "±2",
+		"Texto": "Si uno de tus destinos es exactamente 2 más o 2 menos que el otro",
+		"Lunas": " jugá uno de ellos aquí.",
+		"Título reverso": "1",
+		"Texto reverso": "Repetir"
+	},
+	{
+		"Título": "≤5",
+		"Texto": "Si la suma de tus destinos es 5 o menos",
+		"Lunas": " jugá uno de ellos aquí",
+		"Título reverso": "3",
+		"Texto reverso": "¿Mayor que?"
+	},
+	{
+		"Título": "Único",
+		"Texto": "Si tus dos destinos son diferentes entre sí y de todos los otros destinos visibles",
+		"Lunas": " jugá uno de ellos aquí",
+		"Título reverso": "2",
+		"Texto reverso": "Ciclar"
+	},
+	{
+		"Título": "Entre",
+		"Texto": "Si exactamente un destino visible se ubica entre tus destinos",
+		"Lunas": " jugá uno de ellos aquí.",
+		"Título reverso": "2",
+		"Texto reverso": "¿Igual?"
+	},
+	{
+		"Título": "Separados 4",
+		"Texto": "Si la diferencia entre tus destinos es 4 o más",
+		"Lunas": " jugá uno de ellos aquí.",
+		"Título reverso": "4",
+		"Texto reverso": "Descartar menor"
+	},
+	{
+		"Título": "Par",
+		"Texto": "Si la suma de tus destinos es par",
+		"Lunas": " jugá uno de ellos aquí.",
+		"Título reverso": "3",
+		"Texto reverso": "3-4-5"
+	},
+	{
+		"Título": "3-4-5",
+		"Texto": "Si uno de tus destinos es 3",
+		"Lunas": " 4 o 5",
+		"Título reverso": " y el otro no",
+		"Texto reverso": " juega el 3"
+	},
+	{
+		"Título": "Las horas",
+		"Texto": "Si no podés jugar tu destino en ningún otro lado",
+		"Lunas": " hacelo aquí. Después",
+		"Título reverso": " movelo una carta a la derecha.",
+		"Texto reverso": ""
+	}
+]
