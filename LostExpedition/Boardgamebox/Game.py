29bb5ee4f29dcde3fb807851c7db42d9047560b9
leviatas <31702513+leviatas@users.noreply.github.com>
2019-01-07 11:15:29 -0300

    Create Game.py

---
+++ LostExpedition/Boardgamebox/Game.py
@@ -0,0 +1,20 @@
+import json
+from datetime import datetime
+from random import shuffle
+
+from Boardgamebox.Game import Game as BaseGame
+from SayAnything.Boardgamebox.Player import Player
+from SayAnything.Boardgamebox.Board import Board
+#from Boardgamebox.Board import Board
+#from Boardgamebox.State import State
+
+class Game(BaseGame):
+	def __init__(self, cid, initiator, groupName, tipo = None, modo = None):
+		BaseGame.__init__(self, cid, initiator, groupName, tipo, modo)		
+	
+	# Creacion de player de Say Anything.
+	def add_player(self, uid, name):
+		self.playerlist[uid] = Player(name, uid)
+	def create_board(self):
+		player_number = len(self.playerlist)
+		self.board = Board(player_number, self)
