d89b3e311bd4cd6c75b716b0dd5d14a85c9e26c4
leviatas <31702513+leviatas@users.noreply.github.com>
2019-01-07 11:56:27 -0300

    Update Player.py

--- Arcana/Boardgamebox/Player.py
+++ Arcana/Boardgamebox/Player.py
@@ -4,12 +4,16 @@ from Arcana.Constants.Cards import PLAYERFATETOKENS
 class Player(BasePlayer):
 	def __init__(self, name, uid):		
 		BasePlayer.__init__(self, name, uid)		
-		# Lost Expedition atributes
+		# Tokens que el jugador modificara cuando el resto de jugadores le diga. Marca que posible Fate tiene en mano
 		self.playerFateTokens = PLAYERFATETOKENS[:]
 		self.hiddenPlayerFateTokens = []
+		# Fate Tokens que tiene el jugador en mano
+		self.fateTokens = []
 
-	def print_stats(self):
-		board = "--- Stats Jugador %s ---\n" % self.name
-		board += "--- %s 🍲 ---\n" % self.food
-		
+	def print(self):
+		board = "--- Stats Jugador {} ---\n".format(self.name)
+		board += "--- Tokens Visibles ---\n"
+		for fateToken in self.playerFateTokens:
+			board += "{0}({1}) ".format(fateToken["Texto"], fateToken["TimeSymbols"])
+		board = board[:-1]
 		return board
