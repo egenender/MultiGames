f540dd9246b73c75dd31218d6e8a88ecdbddf2ea
leviatas <31702513+leviatas@users.noreply.github.com>
2019-01-07 11:17:14 -0300

    Update Board.py

--- Boardgamebox/Board.py
+++ Boardgamebox/Board.py
@@ -1,4 +1,4 @@
-from Constants.Cards import cartas_aventura
+
 
 import random
 from Boardgamebox.State import State
@@ -6,60 +6,15 @@ from Boardgamebox.State import State
 class Board(object):
     def __init__(self, playercount, game):
         self.state = State()
-        self.num_players = playercount
-        #Lost Expedition        
-        if game.tipo == "LostExpedition":
-            self.cartasAventura = random.sample([*cartas_aventura], len([*cartas_aventura]))
-            self.cartasExplorationActual = []
+        self.num_players = playercount        
         # Cantidad de veces que se mezclo el mazo.
         self.amount_shuffled = 0
         # Se comienza en el primer lugar
         self.progreso = 1
         self.objetivoprogreso = 9
-        
-        self.cartas = []
-        '''                
-        self.exploradores_team1 = {
-            "Campero" : {
-                "vida" : "3", #Comandos con indicaciones pediran al usuario que actuen y luego volvera a la lista de comandos
-                "habilidad" : "Campero",
-                "matable" : True,                
-            },
-            "Brujula" : {
-                "vida" : "3", #Comandos con indicaciones pediran al usuario que actuen y luego volvera a la lista de comandos
-                "habilidad" : "Brujula",
-                "matable" : True,                
-            },
-            "Hoja" : {
-                "vida" : "3", #Comandos con indicaciones pediran al usuario que actuen y luego volvera a la lista de comandos
-                "habilidad" : "Hoja",
-                "matable" : True,                
-            }
-        }
-        
-        self.exploradores_team2 = {
-            "Campero" : {
-                "vida" : "3", #Comandos con indicaciones pediran al usuario que actuen y luego volvera a la lista de comandos
-                "habilidad" : "Campero",
-                "matable" : True,                
-            },
-            "Brujula" : {
-                "vida" : "3", #Comandos con indicaciones pediran al usuario que actuen y luego volvera a la lista de comandos
-                "habilidad" : "Brujula",
-                "matable" : True,                
-            },
-            "Hoja" : {
-                "vida" : "3", #Comandos con indicaciones pediran al usuario que actuen y luego volvera a la lista de comandos
-                "habilidad" : "Hoja",
-                "matable" : True,                
-            }
-        }
-        '''
-        
+        self.cartas = [] 
         self.discards = []
-        self.previous = []        
-        
-        
+        self.previous = []
    
     def print_board(self, game):
         board = ""
