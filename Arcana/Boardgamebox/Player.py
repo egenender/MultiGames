9646026e258b03855296835b3d542cf65b6278e4
leviatas <31702513+leviatas@users.noreply.github.com>
2019-01-07 11:51:40 -0300

    Update Player.py

--- Arcana/Boardgamebox/Player.py
+++ Arcana/Boardgamebox/Player.py
@@ -1,10 +1,12 @@
 from Boardgamebox.Player import Player as BasePlayer
+from Arcana.Constants.Cards import PLAYERFATETOKENS
 
 class Player(BasePlayer):
 	def __init__(self, name, uid):		
 		BasePlayer.__init__(self, name, uid)		
 		# Lost Expedition atributes
-		self.puntaje = 0		
+		self.playerFateTokens = PLAYERFATETOKENS[:]
+		self.hiddenPlayerFateTokens = []
 
 	def print_stats(self):
 		board = "--- Stats Jugador %s ---\n" % self.name
