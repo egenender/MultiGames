1fed0380de2bc281f7a4f608bdfe26f19dd7a5eb
leviatas <31702513+leviatas@users.noreply.github.com>
2019-01-08 14:36:11 -0300

    Update State.py

--- Arcana/Boardgamebox/State.py
+++ Arcana/Boardgamebox/State.py
@@ -1,9 +1,9 @@
 from Boardgamebox.State import State as BaseState
 
-	class State(BaseState):
-		def __init__(self):
-			BaseState.__init__(self)
-			self.doom = None
-			self.score = 0
-			self.topArcana = None
-			self.arcanasOnTable = []
+class State(BaseState):
+	def __init__(self):
+		BaseState.__init__(self)
+		self.doom = None
+		self.score = 0
+		self.topArcana = None
+		self.arcanasOnTable = []
