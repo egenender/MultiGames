abd10cb8b741d19e1048c862d8c45b922bc7b151
leviatas <31702513+leviatas@users.noreply.github.com>
2019-01-09 11:59:29 -0300

    Update Controller.py

--- Arcana/Controller.py
+++ Arcana/Controller.py
@@ -82,7 +82,7 @@ def callback_finish_config(bot, update):
 		cid, strcid, opcion, uid, struid = int(regex.group(1)), regex.group(1), regex.group(2), int(regex.group(3)), regex.group(3)
 		mensaje_edit = "Por la difficultad el doom comienza en: {0}".format(opcion)
 		
-		update.callback_query.answer(text="Seleccionaste Doom X", show_alert=True)
+		update.callback_query.answer(text="Si ningún destino visible es exactamente 1 más o 1 menos que cualquiera de tus destinos, jugá uno de ellos aquí.", show_alert=True)
 		
 		try:
 			bot.edit_message_text(mensaje_edit, cid, callback.message.message_id)
