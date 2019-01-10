a363d394540c46d052a3ea604dec8bb5a2c518fd
egenender <egenender@gmail.com>
2019-01-10 18:41:43 -0300

    Add "Legal" property to Cards
    Every card now has a lambda expression that controls the legality of the fate being played on it.

--- Arcana/Constants/Cards.py
+++ Arcana/Constants/Cards.py
@@ -131,209 +131,239 @@ ARCANACARDS = [
    "Texto": "Si uno de tus destinos es 1, 4 o 7, y el otro no, juega el 1, 4 o 7 aquí.",
    "Lunas": "3",
    "Título reverso": "¿Dentro de 2?",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿La diferencia entre tus destinos era de 2 o menos?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿La diferencia entre tus destinos era de 2 o menos?\".",
+   "Legal": lambda kept, played: played in [1,4,7] and kept not in [1,4,7]
  },
  {
    "Título": "Dentro de 2",
    "Texto": "Si la diferencia entre tus destinos es 1 o 2, jugá uno de ellos aquí",
    "Lunas": "2",
    "Título reverso": "¿Dentro de 2?",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿La diferencia entre tus destinos era de 2 o menos?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿La diferencia entre tus destinos era de 2 o menos?\".",
+   "Legal": lambda kept, played: abs(kept-played) in [1,2]
  },
  {
    "Título": "Levantamiento",
    "Texto": "Si uno de tus destinos es mayor que el destino más alto jugado aquí, jugá el otro destino aquí.",
    "Lunas": "4",
    "Título reverso": "+1",
-   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para tratar el destino no jugado como si fuera 1 más alto durante este turno."
+   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para tratar el destino no jugado como si fuera 1 más alto durante este turno.",
+   "Legal": lambda kept, played: True #FIXME
  },
  {
    "Título": "Solo",
    "Texto": "Si ningún destino visible es exactamente 1 más o 1 menos que cualquiera de tus destinos, jugá uno de ellos aquí.",
    "Lunas": "2",
    "Título reverso": "+1",
-   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para tratar el destino no jugado como si fuera 1 más alto durante este turno."
+   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para tratar el destino no jugado como si fuera 1 más alto durante este turno.",
+   "Legal": lambda kept, played: True #FIXME
  },
  {
    "Título": "Igual",
    "Texto": "Si tus dos destinos son iguales, jugá uno de ellos aquí.",
    "Lunas": "1",
    "Título reverso": "Repetir",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para hacer una predicción extra este turno."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para hacer una predicción extra este turno.",
+   "Legal": lambda kept, played: played == kept
  },
  {
    "Título": "Todos x4",
    "Texto": "Si la suma de tus destinos más todos los destinos visibles es un múltiplo de 4, jugá uno de tus destinos aquí.",
    "Lunas": "3",
    "Título reverso": "Reubicar",
-   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para mover un destino visible a una nueva carta."
+   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para mover un destino visible a una nueva carta.",
+   "Legal": lambda kept, played: True #FIXME
  },
  {
    "Título": "Altas probabilidades",
    "Texto": "Si tus destinos son diferentes y ambos impares, jugá el más alto aquí. Si solo uno es impar, jugá el más bajo aquí.",
    "Lunas": "3",
    "Título reverso": "+1",
-   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para tratar el destino no jugado como si fuera 1 más alto durante este turno."
+   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para tratar el destino no jugado como si fuera 1 más alto durante este turno.",
+   "Legal": lambda kept, played: (not (played in [2,4,6] and kept in [2,4,6])) and ((played in [1,3,5,7] and kept in [1,3,5,7] and played > kept) or (kept < played))
  },
  {
    "Título": "±1",
    "Texto": "Si uno de tus destinos es exactamente 1 más o 1 menos que el otro, jugá uno de ellos aquí.",
    "Lunas": "1",
    "Título reverso": "Repetir",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para hacer una predicción extra este turno."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para hacer una predicción extra este turno.",
+   "Legal": lambda kept, played: abd(played - kept) == 1
  },
  {
    "Título": "Menor",
    "Texto": "Si uno de tus destinos es menor que el otro, jugá el menor aquí",
    "Lunas": "3",
    "Título reverso": "¿Mayor que?",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para elegir un número X y preguntar \"¿Tu destino es mayor que X?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para elegir un número X y preguntar \"¿Tu destino es mayor que X?\".",
+   "Legal": lambda kept, played: played < kept
  },
  {
    "Título": "Libre",
    "Texto": "Colocá uno de tus destinos aquí. No avances la condena cuando me desvanezca.",
    "Lunas": "1",
    "Título reverso": "1-4-7",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 1, 4 o 7?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 1, 4 o 7?\".",
+   "Legal": lambda kept, played: True
  },
  {
    "Título": "≥11",
    "Texto": "Si la suma de tus destinos es 11 o más, jugá uno de ellos aquí",
    "Lunas": "4",
    "Título reverso": "Descartar menor",
-   "Texto reverso": "Después de jugar su destino, el jugador activo puede descartar esta carta para descartar un destino visible que sea menor que el destino restante."
+   "Texto reverso": "Después de jugar su destino, el jugador activo puede descartar esta carta para descartar un destino visible que sea menor que el destino restante.",
+   "Legal": lambda kept, played: played + kept >= 11
  },
  {
    "Título": "Coincidencia",
    "Texto": "Si uno de tus destinos coincide con uno visible, jugá tu otro destino aquí.",
    "Lunas": "2",
    "Título reverso": "Ciclar",
-   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para reemplazar una carta arcana (sin destinos) por la carta superior del mazo."
+   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para reemplazar una carta arcana (sin destinos) por la carta superior del mazo.",
+   "Legal": lambda kept, played: True #FIXME
  },
  {
    "Título": "x3",
    "Texto": "Si la suma de tus destinos es un múltiplo de 3, jugá uno de ellos aquí.",
    "Lunas": "2",
    "Título reverso": "Descartar menor",
-   "Texto reverso": "Después de jugar su destino, el jugador activo puede descartar esta carta para descartar un destino visible que sea menor que el destino restante."
+   "Texto reverso": "Después de jugar su destino, el jugador activo puede descartar esta carta para descartar un destino visible que sea menor que el destino restante.",
+   "Legal": lambda kept, played: (played + kept) % 3 == 0
  },
  {
    "Título": "5-6-7",
    "Texto": "Si uno de tus destinos es 5, 6 o 7, y el otro no, juega el 5, 6 o 7 aquí.",
    "Lunas": "3",
    "Título reverso": "5-6-7",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 5, 6 o 7?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 5, 6 o 7?\".",
+   "Legal": lambda kept, played: played in [5,6,7] and kept not in [5,6,7]
  },
  {
    "Título": "7 a 9",
    "Texto": "Si la suma de tus destinos es 7, 8 o 9, jugá uno de ellos aquí.",
    "Lunas": "3",
    "Título reverso": "¿Viejo?",
-   "Texto reverso": "Después de jugar su destino, el jugador activo puede descartar esta carta para decir si tenía ese destino desde el turno anterior."
+   "Texto reverso": "Después de jugar su destino, el jugador activo puede descartar esta carta para decir si tenía ese destino desde el turno anterior.",
+   "Legal": lambda kept, played: (played + kept) in [7,8,9]
  },
  {
    "Título": "Adivinar",
    "Texto": "Colocá uno de tus destinos aquí. Si lo hacés, tus compañeros pueden hacer una predicción extra este turno.",
    "Lunas": "2",
    "Título reverso": "¿Viejo?",
-   "Texto reverso": "Después de jugar su destino, el jugador activo puede descartar esta carta para decir si tenía ese destino desde el turno anterior."
+   "Texto reverso": "Después de jugar su destino, el jugador activo puede descartar esta carta para decir si tenía ese destino desde el turno anterior.",
+   "Legal": lambda kept, played: True
  },
  {
    "Título": "Diferencia",
    "Texto": "Si la diferencia entre tus destinos es igual a un destino visible, jugá el mayor aquí",
    "Lunas": "1",
    "Título reverso": "¿Igual?",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para elegir un destino visible y preguntar \"¿Tu destino es igual?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para elegir un destino visible y preguntar \"¿Tu destino es igual?\".",
+   "Legal": lambda kept, played: True #FIXME
  },
  {
    "Título": "Factor",
    "Texto": "Si uno de tus destinos es el doble o el triple del otro, jugá uno de ellos aquí.",
    "Lunas": "3",
    "Título reverso": "5-6-7",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 5, 6 o 7?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 5, 6 o 7?\".",
+   "Legal": lambda kept, played: played == 2*kept or played == 3*kept or kept == 2*played or kept == 3*played
  },
  {
    "Título": "1-2-3",
    "Texto": "Si uno de tus destinos es 1, 2 o 3, y el otro no, juega el 1, 2 o 3 aquí.",
    "Lunas": "2",
    "Título reverso": "1-2-3",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 1, 2 o 3?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 1, 2 o 3?\".",
+   "Legal": lambda kept, played: played in [1,2,3] and kept not in [1,2,3]
  },
  {
    "Título": "Sacar",
    "Texto": "Colocá uno de tus destinos aquí, sacá otro destino y continuá tu turno. No podés jugar aquí nuevamente.",
    "Lunas": "4",
    "Título reverso": "Reubicar",
-   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para mover un destino visible a una nueva carta."
+   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para mover un destino visible a una nueva carta.",
+   "Legal": lambda kept, played: True #FIXME
  },
  {
    "Título": "Decir",
    "Texto": "Colocá uno de tus destinos aquí. Si lo hacés, decí si tu destino restante es mayor que el que jugaste.",
    "Lunas": "2",
    "Título reverso": "1-4-7",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 1, 4 o 7?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 1, 4 o 7?\".",
+   "Legal": lambda kept, played: True
  },
  {
    "Título": "Impar",
    "Texto": "Si la suma de tus destinos es impar, jugá uno de ellos aquí.",
    "Lunas": "3",
    "Título reverso": "1-2-3",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 1, 2 o 3?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 1, 2 o 3?\".",
+   "Legal": lambda kept, played: (played + kept) % 2 != 0
  },
  {
    "Título": "Mayor",
    "Texto": "Si uno de tus destinos es mayor que el otro, jugá el mayor aquí",
    "Lunas": "3",
    "Título reverso": "¿Mayor que?",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para elegir un número X y preguntar \"¿Tu destino es mayor que X?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para elegir un número X y preguntar \"¿Tu destino es mayor que X?\".",
+   "Legal": lambda kept, played: played > kept
  },
  {
    "Título": "±2",
    "Texto": "Si uno de tus destinos es exactamente 2 más o 2 menos que el otro, jugá uno de ellos aquí.",
    "Lunas": "1",
    "Título reverso": "Repetir",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para hacer una predicción extra este turno."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para hacer una predicción extra este turno.",
+   "Legal": lambda kept, played: abs(played - kept) == 2
  },
  {
    "Título": "≤5",
    "Texto": "Si la suma de tus destinos es 5 o menos, jugá uno de ellos aquí",
    "Lunas": "3",
    "Título reverso": "¿Mayor que?",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para elegir un número X y preguntar \"¿Tu destino es mayor que X?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para elegir un número X y preguntar \"¿Tu destino es mayor que X?\".",
+   "Legal": lambda kept, played: played + kept <= 5
  },
  {
    "Título": "Único",
    "Texto": "Si tus dos destinos son diferentes entre sí y de todos los otros destinos visibles, jugá uno de ellos aquí",
    "Lunas": "2",
    "Título reverso": "Ciclar",
-   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para reemplazar una carta arcana (sin destinos) por la carta superior del mazo."
+   "Texto reverso": "Antes de jugar el destino, el jugador activo puede descartar esta carta para reemplazar una carta arcana (sin destinos) por la carta superior del mazo.",
+   "Legal": lambda kept, played: played != kept #FIXME
  },
  {
    "Título": "Entre",
    "Texto": "Si exactamente un destino visible se ubica entre tus destinos, jugá uno de ellos aquí.",
    "Lunas": "2",
    "Título reverso": "¿Igual?",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para elegir un destino visible y preguntar \"¿Tu destino es igual?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para elegir un destino visible y preguntar \"¿Tu destino es igual?\".",
+   "Legal": lambda kept, played: True #FIXME
  },
  {
    "Título": "Separados 4",
    "Texto": "Si la diferencia entre tus destinos es 4 o más, jugá uno de ellos aquí.",
    "Lunas": "4",
    "Título reverso": "Descartar menor",
-   "Texto reverso": "Después de jugar su destino, el jugador activo puede descartar esta carta para descartar un destino visible que sea menor que el destino restante."
+   "Texto reverso": "Después de jugar su destino, el jugador activo puede descartar esta carta para descartar un destino visible que sea menor que el destino restante.",
+   "Legal": lambda kept, played: abs(played - kept) >= 4
  },
  {
    "Título": "Par",
    "Texto": "Si la suma de tus destinos es par, jugá uno de ellos aquí.",
    "Lunas": "3",
    "Título reverso": "3-4-5",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 3, 4 o 5?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 3, 4 o 5?\".",
+   "Legal": lambda kept, played: (played + kept) % 2 == 0
  },
  {
    "Título": "3-4-5",
    "Texto": "Si uno de tus destinos es 3, 4 o 5, y el otro no, juega el 3, 4 o 5 aquí.",
    "Lunas": "3",
    "Título reverso": "3-4-5",
-   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 3, 4 o 5?\"."
+   "Texto reverso": "Antes de hacer una predicción, el grupo puede descartar esta carta para preguntar \"¿Tu destino es 3, 4 o 5?\".",
+   "Legal": lambda kept, played: played in [3,4,5] and kept not in [3,4,5]
  },
 ]
